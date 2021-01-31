3592fb96b5a84b4ff3151cb588dcc06fd0a060d6
Tiffany Le <70486173+bunnyangel@users.noreply.github.com>
2021-01-30 23:17:56 -0800

    Grow a Garden
    Created clickable grid that represents a garden plot; users can choose dimensions of garden (10 x 10 tiles max)

---
+++ script.js
@@ -0,0 +1,29 @@
+var x = window.prompt("Enter number of rows (up to 10): ");
+var y = window.prompt("Enter number of columns (up to 10): ");
+var lastClicked;
+var grid = clickableGrid(x,y,function(el,row,col,i){
+    el.className='clicked';
+    if (lastClicked) lastClicked.className='';
+    lastClicked = el;
+});
+
+document.body.appendChild(grid);
+     
+function clickableGrid( rows, cols, callback ){
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    if (rows <= 10 && cols <= 10){
+      for (var r=0;r<rows;++r){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var cell = tr.appendChild(document.createElement('td'));
+            cell.addEventListener('click',(function(el,r,c){
+                return function(){
+                    callback(el,r,c);
+                }
+            })(cell,r,c),false);
+        }
+      }
+    }
+    return grid;
+}
